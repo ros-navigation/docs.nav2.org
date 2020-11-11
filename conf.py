51f873115238114f3309b6d57eaf3bd6c593815d
Steve Macenski <stevenmacenski@gmail.com>
2020-11-11 15:20:02 -0800

    adding new logo! (#112)

--- conf.py
+++ conf.py
@@ -148,8 +148,8 @@ html_context = {
 #
 # html_theme_options = {}
 
-html_logo = 'images/logo_white_200w.png'
-html_favicon = 'images/favicon-48x48.png'
+html_logo = 'images/nav2_logo.png'
+html_favicon = 'images/nav2_48x48.png'
 
 numfig = True
 #numfig_secnum_depth = (2)
