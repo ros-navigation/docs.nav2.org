e385b01fd820094e29cb05018181e317ef5e764b
ruffsl <roxfoxpox@gmail.com>
2023-04-19 15:46:53 +0200

    Exclude README file from markdown pattern to fix warnings

--- conf.py
+++ conf.py
@@ -89,7 +89,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build','_themes','scripts' ]
+exclude_patterns = ['_build','_themes','scripts', 'README.md' ]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
